eedad9be21cd2c10dbe342071cf01089c9d0855b
Robert DiBiano <16107527+DiBianoR@users.noreply.github.com>
2026-07-24 11:55:38 -0500

    Update universal_agent_2_of_3.js

--- universal_agent_2_of_3.js
+++ universal_agent_2_of_3.js
@@ -35,6 +35,107 @@
     },
     {
       "parameters": {
+        "rules": {
+          "values": [
+            {
+              "conditions": {
+                "options": {
+                  "caseSensitive": true,
+                  "leftValue": "",
+                  "typeValidation": "strict",
+                  "version": 2
+                },
+                "conditions": [
+                  {
+                    "id": "s1",
+                    "leftValue": "={{ $json.auth_mode }}",
+                    "rightValue": "query_key",
+                    "operator": {
+                      "type": "string",
+                      "operation": "equals"
+                    }
+                  }
+                ],
+                "combinator": "and"
+              },
+              "renameOutput": true,
+              "outputKey": "query_key"
+            },
+            {
+              "conditions": {
+                "options": {
+                  "caseSensitive": true,
+                  "leftValue": "",
+                  "typeValidation": "strict",
+                  "version": 2
+                },
+                "conditions": [
+                  {
+                    "id": "s2",
+                    "leftValue": "={{ $json.auth_mode }}",
+                    "rightValue": "bearer",
+                    "operator": {
+                      "type": "string",
+                      "operation": "equals"
+                    }
+                  }
+                ],
+                "combinator": "and"
+              },
+              "renameOutput": true,
+              "outputKey": "bearer"
+            }
+          ]
+        },
+        "options": {}
+      },
+      "type": "n8n-nodes-base.switch",
+      "typeVersion": 3.2,
+      "position": [
+        624,
+        608
+      ],
+      "id": "678fb777-685e-4cf0-9b32-728e57494573",
+      "name": "Provider Switch"
+    },
+    {
+      "parameters": {
+        "method": "POST",
+        "url": "={{ $json.model_url }}",
+        "sendHeaders": true,
+        "headerParameters": {
+          "parameters": [
+            {
+              "name": "Content-Type",
+              "value": "application/json"
+            },
+            {
+              "name": "Authorization",
+              "value": "=Bearer {{ $json.api_key }}"
+            }
+          ]
+        },
+        "sendBody": true,
+        "specifyBody": "json",
+        "jsonBody": "={{ $json.requestBody }}",
+        "options": {
+          "timeout": 180000
+        }
+      },
+      "type": "n8n-nodes-base.httpRequest",
+      "typeVersion": 4.2,
+      "position": [
+        848,
+        688
+      ],
+      "id": "6d538c70-b9c8-4dd0-a816-382da89b4888",
+      "name": "2b. API Call (OpenAI)",
+      "retryOnFail": true,
+      "maxTries": 3,
+      "waitBetweenTries": 2000
+    },
+    {
+      "parameters": {
         "method": "POST",
         "url": "={{ $json.model_url }}",
         "sendQuery": true,
@@ -69,7 +170,7 @@
         528
       ],
       "id": "bf382e6b-8e23-4569-be35-b8feab8c2948",
-      "name": "2. API Call",
+      "name": "2. API Call(Gemini)",
       "retryOnFail": true,
       "maxTries": 3,
       "waitBetweenTries": 2000
@@ -81,14 +182,37 @@
         [],
         [
           {
-            "node": "2. API Call",
+            "node": "Provider Switch",
             "type": "main",
             "index": 0
           }
         ]
       ]
     },
-    "2. API Call": {
+    "Provider Switch": {
+      "main": [
+        [
+          {
+            "node": "2. API Call(Gemini)",
+            "type": "main",
+            "index": 0
+          }
+        ],
+        [
+          {
+            "node": "2b. API Call (OpenAI)",
+            "type": "main",
+            "index": 0
+          }
+        ]
+      ]
+    },
+    "2b. API Call (OpenAI)": {
+      "main": [
+        []
+      ]
+    },
+    "2. API Call(Gemini)": {
       "main": [
         []
       ]
